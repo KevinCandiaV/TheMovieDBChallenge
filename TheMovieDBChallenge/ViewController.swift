bb980877f6ae532a2bf071f06207760615c04244
Kevin Candia Villagómez <kevin_6_14@hotmail.com>
2023-12-06 11:55:07 -0500

    adding MARKS for develop

--- TheMovieDBChallenge/ViewController.swift
+++ TheMovieDBChallenge/ViewController.swift
@@ -10,24 +10,42 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var ejemploTableview: UITableView!
+    
+    //MARK: Espacio para crear lista
+//    var numeroLista = [1,2,3,4,5,6,7,8,9,10]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // MARK: - LLamar metodos creados
     }
     
+    // MARK: - Borrar metodo y ordenar
     func metodo1() {
         ejemploTableview.delegate = self
         ejemploTableview.dataSource = self
     }
     
+    // MARK: - Crear metodo setupUI()
+    // configurar vista de color a eleccion libre
+    
+    
+    // MARK: - Crear metodo setupTableView()
+    // agregar delegados
+    
+    // MARK: - Crear metodo regiterTable()
+    // registar celda
+    
 }
 
 extension ViewController: UITableViewDelegate {
+    // MARK: - Agregar metodo didSelectRowAt
     
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // MARK: - Contar elementos de la lista
+        // numeroLista.count
         return 10
     }
     
