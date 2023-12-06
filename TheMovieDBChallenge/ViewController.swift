6dcc1915e95a560e2ceb450d5ca3af1daecfde48
Kevin Candia Villagómez <kevin.candia@softtek.com>
2023-12-06 00:12:39 -0500

    se agrego funcion delegados y extension del controlador

--- TheMovieDBChallenge/ViewController.swift
+++ TheMovieDBChallenge/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ejemploTableview: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func metodo1() {
+        ejemploTableview.delegate = self
+        ejemploTableview.dataSource = self
+    }
+    
+}
 
+extension ViewController: UITableViewDelegate {
+    
+}
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda = UITableViewCell()
+        return celda
+    }
+    
+    
 }
 
