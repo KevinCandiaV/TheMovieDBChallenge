de3d47a36ba39743541b72b523e0d65b39289c91
Kevin Candia Villagómez <kevin_6_14@hotmail.com>
2023-12-06 11:50:35 -0500

    adding tableView with Data and logic

--- TheMovieDBChallenge/ViewController.swift
+++ TheMovieDBChallenge/ViewController.swift
@@ -8,34 +8,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var ejemploTableview: UITableView!
+    
+    // Datos de ejemlo para la lista
+    let datosLista = [
+        ("Elemento 1", "Subtítulo 1", "2023-01-01"),
+        ("Elemento 2", "Subtítulo 2", "2023-02-15"),
+        ("Elemento 3", "Subtítulo 3", "2023-03-30")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+        setupTable()
+        registerTable()
     }
     
-    func metodo1() {
+    func setupUI() {
+        view.backgroundColor = .gray
+    }
+    
+    func setupTable() {
         ejemploTableview.delegate = self
         ejemploTableview.dataSource = self
+    }
+    
+    func registerTable() {
+        ejemploTableview.register(UITableViewCell.self, forCellReuseIdentifier: "celdaID")
     }
     
 }
 
 extension ViewController: UITableViewDelegate {
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return datosLista.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let celda = UITableViewCell()
-        return celda
+        if indexPath.row == datosLista.count - 1 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "celdaID", for: indexPath)
+            
+            let (titulo, subtitulo, fecha) = datosLista[indexPath.row]
+            cell.textLabel?.text = "\(titulo) - \(subtitulo) - Fecha: \(fecha)"
+            cell.backgroundColor = .blue
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "celdaID", for: indexPath)
+            
+            let (titulo, subtitulo, fecha) = datosLista[indexPath.row]
+            cell.textLabel?.text = "\(titulo) - \(subtitulo) - Fecha: \(fecha)"
+            return cell
+        }
     }
-    
     
 }
 
