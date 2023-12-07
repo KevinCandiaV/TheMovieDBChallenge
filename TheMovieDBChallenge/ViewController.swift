2899a40a2aff961ff073717f1279139b5b4f3164
Kevin Candia Villagómez <kevin.candia@softtek.com>
2023-12-06 23:27:37 -0500

    crear lista numerica

--- TheMovieDBChallenge/ViewController.swift
+++ TheMovieDBChallenge/ViewController.swift
@@ -12,33 +12,42 @@ class ViewController: UIViewController {
     @IBOutlet weak var ejemploTableview: UITableView!
     
     //MARK: Espacio para crear lista
-//    var numeroLista = [1,2,3,4,5,6,7,8,9,10]
+    var datitosLista = ["1","2","3","4","5"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // MARK: - LLamar metodos creados
-    }
-    
-    // MARK: - Borrar metodo y ordenar
-    func metodo1() {
-        ejemploTableview.delegate = self
-        ejemploTableview.dataSource = self
+        setupUI()
+        setupTableView()
+        registerTable()
     }
     
     // MARK: - Crear metodo setupUI()
     // configurar vista de color a eleccion libre
-    
+    func setupUI() {
+        view.backgroundColor = .cyan
+    }
     
     // MARK: - Crear metodo setupTableView()
     // agregar delegados
+    func setupTableView() {
+        ejemploTableview.delegate = self
+        ejemploTableview.dataSource =  self
+    }
     
     // MARK: - Crear metodo regiterTable()
     // registar celda
+    func registerTable() {
+        ejemploTableview.register(UITableViewCell.self, forCellReuseIdentifier: "celdita")
+    }
     
 }
 
 extension ViewController: UITableViewDelegate {
     // MARK: - Agregar metodo didSelectRowAt
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
     
 }
 
@@ -46,11 +55,12 @@ extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // MARK: - Contar elementos de la lista
         // numeroLista.count
-        return 10
+        return datitosLista.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let celda = UITableViewCell()
+        let celda = tableView.dequeueReusableCell(withIdentifier: "celdita", for: indexPath)
+        celda.textLabel?.text = datitosLista[indexPath.row]
         return celda
     }
     
